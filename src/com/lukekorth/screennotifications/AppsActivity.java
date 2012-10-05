14f53389b7aee497f33da7d17630e0d4daf7e4a1
lkorth <luke@lukekorth.com>
2012-10-05 07:58:44 -0500

    Async load app list

--- src/com/lukekorth/screennotifications/AppsActivity.java
+++ src/com/lukekorth/screennotifications/AppsActivity.java
@@ -1,49 +1,68 @@
 package com.lukekorth.screennotifications;
 
-import java.util.Collections;
-import java.util.List;
-
+import android.app.ProgressDialog;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 
+import java.util.Collections;
+import java.util.List;
+
 public class AppsActivity extends PreferenceActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		setPreferenceScreen(createPreferenceHierarchy());
-	}
+        AppLoader task = new AppLoader();
+        task.execute();
+    }
 
-	private PreferenceScreen createPreferenceHierarchy() {
-		// Root
-		PreferenceScreen root = getPreferenceManager().createPreferenceScreen(this);
+    private class AppLoader extends AsyncTask<Void, Void, Void>{
 
-		// Inline preferences
-		PreferenceCategory inlinePrefCat = new PreferenceCategory(this);
-		inlinePrefCat.setTitle("Apps");
-		root.addPreference(inlinePrefCat);
+        ProgressDialog loadingDialog;
+        PreferenceScreen root;
 
-		final PackageManager pm = getPackageManager();
-		//get a list of installed apps
-		List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-		Collections.sort(packages, new ApplicationInfo.DisplayNameComparator(pm));
+        protected void onPreExecute() {
+            loadingDialog = ProgressDialog.show(AppsActivity.this, "", "Loading. Please wait...", true);
+        }
 
-		for (ApplicationInfo packageInfo : packages) {
-			// Checkbox preference
-			CheckBoxPreference checkboxPref = new CheckBoxPreference(this);
-			checkboxPref.setKey(packageInfo.packageName);
-			checkboxPref.setTitle(packageInfo.loadLabel(pm));
-			inlinePrefCat.addPreference(checkboxPref);
-		}
+        @Override
+        protected Void doInBackground(Void... nothing) {
+            // Root
+            root = getPreferenceManager().createPreferenceScreen(AppsActivity.this);
 
-		return root;
-	}
+            // Inline preferences
+            PreferenceCategory inlinePrefCat = new PreferenceCategory(AppsActivity.this);
+            inlinePrefCat.setTitle("Apps");
+            root.addPreference(inlinePrefCat);
 
+            final PackageManager pm = getPackageManager();
+            //get a list of installed apps
+            List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+            Collections.sort(packages, new ApplicationInfo.DisplayNameComparator(pm));
+
+            for (ApplicationInfo packageInfo : packages) {
+                // Checkbox preference
+                CheckBoxPreference checkboxPref = new CheckBoxPreference(AppsActivity.this);
+                checkboxPref.setKey(packageInfo.packageName);
+                checkboxPref.setTitle(packageInfo.loadLabel(pm));
+                inlinePrefCat.addPreference(checkboxPref);
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(Void nothing) {
+            setPreferenceScreen(root);
+            loadingDialog.cancel();
+        }
+
+    }
 
 }
