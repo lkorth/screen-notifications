4a39e699c6392a98ceb4694944f084a7faadf370
lkorth <luke@lukekorth.com>
2012-10-10 20:41:10 -0500

    Fix missing configuration for Accessibility listener on pre-Jelly Bean devices

--- src/com/lukekorth/screennotifications/ScreenNotificationsService.java
+++ src/com/lukekorth/screennotifications/ScreenNotificationsService.java
@@ -1,9 +1,18 @@
 package com.lukekorth.screennotifications;
 
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.view.accessibility.AccessibilityEvent;
+
 public class ScreenNotificationsService extends BaseAccessibilityService {
 
     public void onServiceConnected() {
         super.onServiceConnected();
+
+        AccessibilityServiceInfo localAccessibilityServiceInfo = new AccessibilityServiceInfo();
+        localAccessibilityServiceInfo.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
+        localAccessibilityServiceInfo.feedbackType = 16;
+        localAccessibilityServiceInfo.notificationTimeout = 0L;
+        setServiceInfo(localAccessibilityServiceInfo);
     }
 
 }
