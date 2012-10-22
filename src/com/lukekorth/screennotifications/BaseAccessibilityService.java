7e84d76f55c32d3f31b998e8cd96e0f79923edbf
lkorth <luke@lukekorth.com>
2012-10-21 19:37:21 -0500

    Add quiet hour options

--- src/com/lukekorth/screennotifications/BaseAccessibilityService.java
+++ src/com/lukekorth/screennotifications/BaseAccessibilityService.java
@@ -11,6 +11,9 @@ import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.view.accessibility.AccessibilityEvent;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class BaseAccessibilityService extends AccessibilityService implements SensorEventListener {
 
     private boolean close;
@@ -32,25 +35,58 @@ public class BaseAccessibilityService extends AccessibilityService implements Se
         SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 
         if(mPrefs.getBoolean((String) event.getPackageName(), false)) {
-            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            if(shouldTurnOnScreen(mPrefs)) {
+                PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
 
-            if(!pm.isScreenOn()) {
-                if(mPrefs.getBoolean("proxSensor", false)) {
-                    turnOnScreen(mPrefs, pm);
-
-                    SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-                    sensorManager.unregisterListener(this);
-                }
-                else {
-                    if(close == false)
+                if(!pm.isScreenOn()) {
+                    if(mPrefs.getBoolean("proxSensor", false)) {
                         turnOnScreen(mPrefs, pm);
 
-                    SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-                    Sensor proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-                    sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+                        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+                        sensorManager.unregisterListener(this);
+                    }
+                    else {
+                        if(close == false)
+                            turnOnScreen(mPrefs, pm);
+
+                        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+                        Sensor proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+                        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    }
                 }
             }
         }
+    }
+
+    private boolean shouldTurnOnScreen(SharedPreferences mPrefs) {
+        if(mPrefs.getBoolean("quiet", false)) {
+            String startTime = mPrefs.getString("startTime", "22:00");
+            String stopTime = mPrefs.getString("stopTime", "08:00");;
+            boolean turnOnScreen = true;
+
+            SimpleDateFormat sdfDate = new SimpleDateFormat("H:mm");
+            String currentTimeStamp = sdfDate.format(new Date());
+            int currentHour = Integer.parseInt(currentTimeStamp.split("[:]+")[0]);
+            int currentMinute = Integer.parseInt(currentTimeStamp.split("[:]+")[1]);
+
+            int startHour  = Integer.parseInt(startTime.split("[:]+")[0]);
+            int startMinute = Integer.parseInt(startTime.split("[:]+")[1]);
+
+            int stopHour = Integer.parseInt(stopTime.split("[:]+")[0]);
+            int stopMinute = Integer.parseInt(stopTime.split("[:]+")[1]);
+
+            if(startHour < stopHour && currentHour > startHour && currentHour < stopHour)
+                turnOnScreen = false;
+            else if (startHour > stopHour && (currentHour > startHour || currentHour < stopHour))
+                turnOnScreen = false;
+            else if(currentHour == startHour && currentMinute >= startMinute)
+                turnOnScreen = false;
+            else if(currentHour == stopHour && currentMinute < stopMinute)
+                turnOnScreen = false;
+
+            return turnOnScreen;
+        }
+        return true;
     }
 
     private void turnOnScreen(SharedPreferences mPrefs, PowerManager pm) {
